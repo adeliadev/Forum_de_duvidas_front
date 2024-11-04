eeddaa93159164186f16e2d15229ca8246b4c8c9
Moniquearjm <monique.araujo@cardoo.com.br>
2024-11-04 19:12:54 -0300

    feat home usuario, visitando e sobre

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,16 @@ function toggleLike(element) {
     element.src = unlikedImage;
   }
 }
+// FUNÇÃO PARA ADICIONAR IMAGEM NO AVATAR
+function previewImage(event) {
+  const avatar = document.getElementById('avatar');
+  const file = event.target.files[0];
+
+  if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+          avatar.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+  }
+}
