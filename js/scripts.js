3e8887ea5221c6151053b9542b6049404555291d
adeliadev <arodriguesthamyres@gmail.com>
2024-11-05 16:25:45 -0300

    refactor: Altera caminhos das imagens utilizadas nas páginas

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // FUNÇÃO PARA DAR LIKE NO POST
 function toggleLike(element) {
-  const unlikedImage = "./images/unliked.svg";
-  const likedImage = "./images/liked.svg";
+  const unlikedImage = "./images/postagens/unliked.svg";
+  const likedImage = "./images/postagens/liked.svg";
   if (element.src.includes("unliked.svg")) {
     element.src = likedImage;
   } else {
