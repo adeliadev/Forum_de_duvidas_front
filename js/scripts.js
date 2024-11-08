3908f43618b4bd8df080fb1cee2662f5cf7f1e13
Marcus Vinicius <francisquinimarcus@gmail.com>
2024-11-08 19:10:44 -0300

    refactor: link de arquivos alterados

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // FUNÇÃO PARA DAR LIKE NO POST
 function toggleLike(element) {
-  const unlikedImage = "./images/postagens/unliked.svg";
-  const likedImage = "./images/postagens/liked.svg";
+  const unlikedImage = "../images/postagens/unliked.svg";
+  const likedImage = "../images/postagens/liked.svg";
   if (element.src.includes("unliked.svg")) {
     element.src = likedImage;
   } else {
